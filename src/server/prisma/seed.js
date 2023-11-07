3ebbcefab5ca334d375ea8f0f8fc3e1c25c7124d
zhiarTX <138315070+zhiarTX@users.noreply.github.com>
2023-11-07 13:54:29 -0700

    Update seed.js
    Created a seeding database with 20 students and 5 task

--- src/server/prisma/seed.js
+++ src/server/prisma/seed.js
@@ -1,26 +1,22 @@
 const prisma = require("../prisma");
 
 /** Seeds the database with a user and some tasks */
-const seed = async () => {
-  await prisma.user.create({
-    data: {
-      username: "foo",
-      password: "bar",
-      tasks: {
-        create: [
-          { description: "task 1" },
-          { description: "task 2" },
-          { description: "task 3" },
-        ],
-      },
-    },
+const seed = async (numStudents = 20, taskPerStudent = 5 ) => {
+  for (let i =0; i < numStudents; i++) {
+    const tasks = [];
+    for (let j=0; j < taskPerStudent; j++) {
+      tasks.push({
+        name: `Task ${j+1}`,          //This means that the task will begin at 1 and not 0.
+      })
+    }
+    await prisma.student.create({
+      data: {
+        name: `Student ${i+1}`,   //This means that the student will begin at 1 and not 0.
+        tasks: {
+          create: tasks
+   },
+   },
   });
+  }
 };
-
-seed()
-  .then(async () => await prisma.$disconnect())
-  .catch(async (err) => {
-    console.error(err);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+  

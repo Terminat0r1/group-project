357d1f49b5fd356c80f655b618d98a05509b1cf4
zhiarTX <138315070+zhiarTX@users.noreply.github.com>
2023-11-08 14:44:45 -0700

    Update seed.js
    Fixed error on the first name

--- src/server/prisma/seed.js
+++ src/server/prisma/seed.js
@@ -5,7 +5,7 @@ const student = async (numStudents = 20) => {
   for (let i =0; i < numStudents; i++) {
     await prisma.student.create({
       data: {
-        firtname: `Firstname ${i}`,
+        firstname: `Firstname ${i}`,
         lastname: `Lastname ${i}`,
         email: `student${i}@example.com`,
         gpa: Math.random() * 4,

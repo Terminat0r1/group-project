2b83c1dbb13b5a13c649dd282b8ca02dfcc8e261
zhiarTX <138315070+zhiarTX@users.noreply.github.com>
2023-11-09 13:35:45 -0700

    Update
    removed unused files

--- src/Student/features/tasks/StudentsLists.jsx
+++ src/Student/features/tasks/StudentsLists.jsx
@@ -4,16 +4,13 @@ import { selectToken } from "../auth/authSlice";
 // import Task from "./Task";
 import { useGetTasksQuery } from "./taskSlice";
 
-import "./Tasks.less";
+
 
 /** Main interface for user to interact with their tasks */
 export default function StudentsList() {
   const token = useSelector(selectToken);
   const { data: tasks, isLoading } = useGetTasksQuery();
-
-  if (!token) {
-    return <p>You must be logged in to see your students.</p>;
-  }
+  
 
   return (
     <div className="tasks">
---
+++ src/server/api/studentRoutes.js
@@ -0,0 +1,99 @@
+// const { ServerError } = require("../errors");
+const prisma = require("../prisma");
+
+const router = require("express").Router();
+module.exports = router;
+
+
+
+/** Sends all tasks */
+router.get("/", async (req, res, next) => {
+  try {
+    const tasks = await prisma.task.findMany({
+      where: { userId: res.locals.user.id },
+    });
+    res.json(tasks);
+  } catch (err) {
+    next(err);
+  }
+});
+
+/** Sends all Students */
+router.get("/students", async (req, res, next) => {
+  try {
+    const students = await prisma.student.findMany();
+    res.json(students);
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+/** Sends a single student by id */
+router.get("/students/:id", async (req, res, next) => {
+  try {
+    const id = +req.params.id;
+
+    const task = await prisma.task.findUnique({ where: { id } });
+    validateTask(res.locals.user, task);
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+/** Creates new student */
+router.post("/", async (req, res, next) => {
+  try {
+    const { firstName, lastName, email, imageUrl, gpa } = req.body;
+
+    const newStudent = await prisma.student.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        imageUrl,
+        gpa,
+      }
+    });
+
+    res.json(newStudent);
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+/** Updates a single student by id */
+router.put("/:id", async (req, res, next) => {
+  try {
+    const id = +req.params.id;
+    const { firstName, lastName, email, imageUrl, gpa } = req.body;
+
+    const updatedStudent = await prisma.student.update({
+      where: { id },
+      data: { firstName, lastName, email, imageUrl, gpa }
+    })
+
+    res.json(updatedStudent);
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+/** Deletes single student by id */
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const id = +req.params.id;
+
+    await prisma.student.delete({ where: { id } });
+
+    res.sendStatus(204).json({ message: 'Student has successfully been deleted' });
+  } catch (err) {
+    next(err);
+  }
+});

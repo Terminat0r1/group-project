53c95d0ba278f961d90da59dedaadfe9de1e4e2b
zhiarTX <138315070+zhiarTX@users.noreply.github.com>
2023-11-09 13:49:40 -0700

    Update StudentsLists.jsx
    removed unused imports, they were using tasks

--- src/Student/features/studentData/StudentsLists.jsx
+++ src/Student/features/studentData/StudentsLists.jsx
@@ -1,8 +1,4 @@
 import { useSelector } from "react-redux";
-import { selectToken } from "../auth/authSlice";
-// import NewTask from "./NewTask";
-// import Task from "./Task";
-import { useGetTasksQuery } from "./taskSlice";
 import React from 'react';
 import { Link } from 'react-router-dom';
 const students = require('../../../server/prisma/seed');
